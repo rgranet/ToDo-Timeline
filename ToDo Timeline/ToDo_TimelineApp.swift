d80eeffa66eb9c995110213fe58aa2124eca1e48
Ruben Granet <117757643+rgranet@users.noreply.github.com>
2024-08-22 16:35:44 +0300

    Initial Commit

---
+++ ToDo Timeline/ToDo_TimelineApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDo_TimelineApp.swift
+//  ToDo Timeline
+//
+//  Created by Ruben Granet on 22/08/2024.
+//
+
+import SwiftUI
+
+@main
+struct ToDo_TimelineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
